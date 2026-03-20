bf2e2ce937130505765388a74308db82c8b2dd24
ashrafomid2025 <omidmashraf@gmail.com>
2026-03-20 09:23:52 +0430

    book list component changes

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -22,9 +22,7 @@ new class extends Component
         $this->bookId = null;
         $this->isModalOpen = false;
     }
-    // public function getBooksProperty(){
-    //     $this->books = Book::all();
-    // }
+
 
     public function deleteBtn(){
        $book =  Book::findOrFail($this->bookId);
@@ -36,16 +34,16 @@ new class extends Component
 };
 ?>
 
-<div class="max-w-6xl mx-auto grid grid-cols-1 my-4 md:grid-cols-2 gap-3">
+<div class="max-w-6xl mx-auto grid grid-cols-1 my-4 md:grid-cols-3 p-3 gap-3">
      @foreach ($books as $book)
          <div wire:key="{{ $book->id }}" class="flex flex-col gap-3 rounded-2xl w-full bg-gray-500 px-4 py-5">
             <div class="p-2 rounded-md  text-2xl font-bold bg-linear-60 from-orange-300 to-orange-500 w-fit">
             <i class=" fa-solid fa-book-open"></i>
             </div>
             <h1 class="text-gray-200">{{ $book->title }}</h1>
-            <h1>Author Name: {{ $book->author }}</h1>
+            <h1 >Author Name: {{ $book->author }}</h1>
             <div class="flex justify-between items-center">
-            <span>Rating: {{ $book->rating }}</span>
+            <span class="px-4 py-2 bg-blue-500 text-gray-300 w-fit">Rating: {{ $book->rating }}</span>
             <button wire:click="deleteBook({{ $book->id }})" class="bg-red-500 py-2 px-5 rounded-md text-gray-200">Delete</button>
             </div>
          </div>
